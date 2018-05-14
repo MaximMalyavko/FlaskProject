8de4d2a9110412d562f3d9c3d6bcbc6a14ee8ff5
Maksim_Maliauka <Maksim_Maliauka@epam.com>
2018-05-14 16:43:28 +0300

    Part 8 unfinished

--- app/models.py
+++ app/models.py
@@ -41,10 +41,17 @@ class User(UserMixin, db.Model):
             self.followed.remove(user)
 
     def is_following(self, user):
-        return self.followed.filter(followers.c.followed_id == user.id).coun() > 0
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def __repr__(self):
         return 'Username: {} '.format(self.username)
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
