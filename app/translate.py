22fe582a68d58ca57e8093800366ae7557ced2e2
Maksim_Maliauka <Maksim_Maliauka@epam.com>
2018-08-05 19:22:23 +0300

    15 part - Refactoring 2

--- app/translate.py
+++ app/translate.py
@@ -1,14 +1,14 @@
 import json
 import requests
 from flask_babel import _
-from app import app
+from flask import current_app
 
 
 def translate(text, source_language, dest_language):
-    if 'JA_TRANSLATOR_KEY' not in app.config or not app.config['JA_TRANSLATOR_KEY']:
+    if 'JA_TRANSLATOR_KEY' not in current_app.config or not current_app.config['JA_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured')
     api_request = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key={}&text={}&lang={}'
-    r = requests.get(api_request.format(app.config['JA_TRANSLATOR_KEY'], text, source_language+'-'+dest_language))
+    r = requests.get(api_request.format(current_app.config['JA_TRANSLATOR_KEY'], text, source_language+'-'+dest_language))
     if r.status_code != 200:
         return _('Error: the translation service failed')
     return json.loads(r.content.decode('utf-8-sig'))['text'][0]
